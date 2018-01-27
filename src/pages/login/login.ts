e2d51a9067ff7b01794b081546153f6876813492
Nayana A <nayana.7red@gmail.com>
2018-01-27 20:24:01 +0530

    Google Auth

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import firebase from 'firebase';
 /**
  * Generated class for the LoginPage page.
  *
@@ -14,12 +14,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
-
+  public userProfile:any = null;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    firebase.auth().onAuthStateChanged( user => {
+      if (user) {
+        console.log(user);
+        this.userProfile = user;
+      } else {
+        console.log("There's no user here");
+      }
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  googleLogin():void {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider).then( () => {
+      firebase.auth().getRedirectResult().then( result => {
+        // This gives you a Google Access Token.
+        // You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log(token, user);
+      }).catch(function(error) {
+        // Handle Errors here.
+        console.log(error.message);
+      });
+    });
   }
 
 }
